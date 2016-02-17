f7c936f61a6c71c97034f0c2f635614d2ffb2141
nolimitk <nolimitk@gmail.com>
2016-02-18 00:38:54 +0900

    fix for rename project

--- bin/MemoryAllocatorBenchmark.bat
+++ bin/MemoryAllocatorBenchmark.bat
@@ -1,19 +1,19 @@
 @echo off
-echo Memory Allocator Performance Tester
+echo MMemory Allocator Benchmark
 echo 02.13.2016 nolimitk
 echo GPL
 
 echo ...
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester malloc
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester new
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester concurrency
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester tbb
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester tc
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester malloc -concurrent
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester new -concurrent
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester concurrency -concurrent
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester tbb -concurrent
-for /L %%G IN (1,1,10 ) do MemoryAllocatorPerformanceTester tc -concurrent
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark malloc
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark new
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark concurrency
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark tbb
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark tc
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark malloc -concurrent
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark new -concurrent
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark concurrency -concurrent
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark tbb -concurrent
+for /L %%G IN (1,1,10 ) do MemoryAllocatorBenchmark tc -concurrent
 
 echo done.
 pause
